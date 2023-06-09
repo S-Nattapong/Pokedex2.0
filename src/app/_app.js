556fe44e811e42f725231dea3cc357512f8b30ac
Sieg <nattapong.lao@ku.th>
2023-06-09 15:22:53 +0700

    create

---
+++ src/app/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
